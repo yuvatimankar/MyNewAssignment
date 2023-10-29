bff16b68e04518663d3bd95fb2e73613814ff93e
Yuvati Mankar <yuvatimankar06@gmail.com>
2023-10-29 16:53:31 +0530

    Updated-nw

--- script.sh
+++ script.sh
@@ -1,1 +1,10 @@
+Shared AMI
 $ aws ec2 modify-image-attribute --image-id ami-001cfbbc903f8e1b3 --launch-permission "{\"Add\":[{\"UserId\":\"399451798445\"}]}" --region ap-south-1 --profile source
+
+
+Coped AMI into Second Account
+$ aws ec2 copy-image --source-image-id ami-001cfbbc903f8e1b3 --source-region ap-south-1 --name "Copied AMI" --region ap-south-1 --profile destination --query 'ImageId' --output text
+ami-05263594adc95dd67
+
+
+
