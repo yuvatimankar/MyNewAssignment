ae6b6ac06f2e16ab186bd8a2f4df2c3a5c9b433e
Yuvati Mankar <yuvatimankar06@gmail.com>
2023-10-29 16:38:01 +0530

    Updated

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-aws ec2 modify-image-attribute --image-id $SOURCE_AMI_ID --launch-permission "{\"Add\":[{\"UserId\":\"$DEST_AWS_ACCOUNT_ID\"}]" --region $DEST_REGION --profile source
+$ aws ec2 modify-image-attribute --image-id ami-001cfbbc903f8e1b3 --launch-permission "{\"Add\":[{\"UserId\":\"399451798445\"}]}" --region ap-south-1 --profile source
